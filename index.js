ef213e6525056741a12150a07deaf5d070e0321c
Shubrajit22 <138674279+Shubrajit22@users.noreply.github.com>
2023-09-06 11:38:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+document.querySelector("button").addEventListener("click",function(){
+  
+          document.querySelector("button").innerHTML= "sent";
+          alert("Thank you....you will soon get notified... ");    });
